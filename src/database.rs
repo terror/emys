dbee6ebf068e189495d26f6433ea86a121a4a9dd
Liam <liam@scalzulli.com>
2026-08-03 18:05:21 -0400

    Reorder Database conversion implementations

--- src/database.rs
+++ src/database.rs
@@ -313,29 +313,6 @@ impl Database {
   }
 }
 
-impl TryFrom<&Path> for Database {
-  type Error = Error;
-
-  fn try_from(path: &Path) -> Result<Self> {
-    #[cfg(unix)]
-    let directory = path
-      .parent()
-      .context("database path has no parent directory")?;
-
-    #[cfg(unix)]
-    fs::set_permissions(directory, fs::Permissions::from_mode(0o700))?;
-
-    let database = Self::open(path).with_context(|| {
-      format!("failed to open database `{}`", path.display())
-    })?;
-
-    #[cfg(unix)]
-    fs::set_permissions(path, fs::Permissions::from_mode(0o600))?;
-
-    Ok(database)
-  }
-}
-
 impl TryFrom<Connection> for Database {
   type Error = Error;
 
@@ -383,6 +360,29 @@ impl TryFrom<Connection> for Database {
     transaction.commit()?;
 
     Ok(Self { connection })
+  }
+}
+
+impl TryFrom<&Path> for Database {
+  type Error = Error;
+
+  fn try_from(path: &Path) -> Result<Self> {
+    #[cfg(unix)]
+    let directory = path
+      .parent()
+      .context("database path has no parent directory")?;
+
+    #[cfg(unix)]
+    fs::set_permissions(directory, fs::Permissions::from_mode(0o700))?;
+
+    let database = Self::open(path).with_context(|| {
+      format!("failed to open database `{}`", path.display())
+    })?;
+
+    #[cfg(unix)]
+    fs::set_permissions(path, fs::Permissions::from_mode(0o600))?;
+
+    Ok(database)
   }
 }
 
