f8682d1f27187d20938b92c301b8108b69daa71c
Liam <liam@scalzulli.com>
2026-08-03 22:59:16 -0400

    Rename crate to honu

--- src/shell/zsh/init.zsh
+++ src/shell/zsh/init.zsh
@@ -1,48 +1,48 @@
 autoload -Uz add-zsh-hook
 zmodload zsh/datetime 2>/dev/null
 
-if [[ -z "${EMYS_SESSION:-}" || "${EMYS_SHLVL:-}" != "$SHLVL" ]]; then
-  typeset -gx EMYS_SESSION="${HOST:-unknown}:$$:${EPOCHREALTIME:-0}"
-  typeset -gx EMYS_SHLVL="$SHLVL"
+if [[ -z "${HONU_SESSION:-}" || "${HONU_SHLVL:-}" != "$SHLVL" ]]; then
+  typeset -gx HONU_SESSION="${HOST:-unknown}:$$:${EPOCHREALTIME:-0}"
+  typeset -gx HONU_SHLVL="$SHLVL"
 fi
 
-typeset -g __emys_command=""
-typeset -g __emys_directory=""
-typeset -g __emys_started_at=""
+typeset -g __honu_command=""
+typeset -g __honu_directory=""
+typeset -g __honu_started_at=""
 
-_emys_preexec() {
+_honu_preexec() {
   emulate -L zsh
 
-  __emys_command="$1"
-  __emys_directory="$PWD"
-  __emys_started_at="${EPOCHREALTIME:-}"
+  __honu_command="$1"
+  __honu_directory="$PWD"
+  __honu_started_at="${EPOCHREALTIME:-}"
 }
 
-_emys_precmd() {
+_honu_precmd() {
   local exit_code="$?"
   emulate -L zsh
 
   local finished_at="${EPOCHREALTIME:-}"
 
-  if [[ -z "$__emys_command" ]]; then
+  if [[ -z "$__honu_command" ]]; then
     return "$exit_code"
   fi
 
-  local command="$__emys_command"
-  local directory="$__emys_directory"
-  local started_at="$__emys_started_at"
+  local command="$__honu_command"
+  local directory="$__honu_directory"
+  local started_at="$__honu_started_at"
   local timestamp_ns=""
   local duration_ns=""
   local -a arguments=(
     --directory "$directory"
     --exit-code "$exit_code"
-    --session "$EMYS_SESSION"
+    --session "$HONU_SESSION"
     --shell zsh
   )
 
-  __emys_command=""
-  __emys_directory=""
-  __emys_started_at=""
+  __honu_command=""
+  __honu_directory=""
+  __honu_started_at=""
 
   if [[ -n "$started_at" && -n "$finished_at" ]]; then
     printf -v timestamp_ns '%.0f' "$((started_at * 1000000000))"
@@ -54,18 +54,18 @@ _emys_precmd() {
     arguments+=(--hostname "$HOST")
   fi
 
-  command emys add "${arguments[@]}" -- "$command" >/dev/null 2>&1
+  command honu add "${arguments[@]}" -- "$command" >/dev/null 2>&1
 
   return "$exit_code"
 }
 
-_emys_search() {
+_honu_search() {
   emulate -L zsh
 
   zle -I
 
   local selected
-  selected="$(command emys search --interactive -- "$BUFFER")"
+  selected="$(command honu search --interactive -- "$BUFFER")"
   local exit_code="$?"
 
   if (( exit_code == 0 )) && [[ -n "$selected" ]]; then
@@ -78,7 +78,7 @@ _emys_search() {
   return "$exit_code"
 }
 
-add-zsh-hook preexec _emys_preexec
-add-zsh-hook precmd _emys_precmd
-zle -N emys-search _emys_search
-bindkey '^R' emys-search
+add-zsh-hook preexec _honu_preexec
+add-zsh-hook precmd _honu_precmd
+zle -N honu-search _honu_search
+bindkey '^R' honu-search
