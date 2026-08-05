230e16cec3521836dbbc372d12e072f4b2becf31
Liam <liam@scalzulli.com>
2026-08-04 23:49:08 -0400

    Test reimport after clear

--- tests/clear.rs
+++ tests/clear.rs
@@ -2,12 +2,22 @@ use super::*;
 
 #[test]
 fn clear() {
-  Test::new()
+  let test = Test::new()
     .write("history", "foo\n")
     .arguments(["import", "--path", "history", "zsh"])
     .stdout("imported 1 executions from history\n")
-    .success()
+    .success();
+
+  let id = test.execution_id("foo");
+
+  let test = test
     .argument("clear")
     .success()
-    .assert_executions([]);
+    .assert_executions([])
+    .arguments(["import", "--path", "history", "zsh"])
+    .stdout("imported 1 executions from history\n")
+    .success()
+    .assert_execution_count(1);
+
+  assert_ne!(test.execution_id("foo"), id);
 }
