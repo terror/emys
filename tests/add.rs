616e0f73f13c64cf20469ac8eab1437f3024ebe2
Liam <liam@scalzulli.com>
2026-08-04 23:06:35 -0400

    Simplify add defaults test

--- tests/add.rs
+++ tests/add.rs
@@ -2,30 +2,10 @@ use super::*;
 
 #[test]
 fn defaults() {
-  let test = Test::new().arguments(["add", "--", "foo"]).success();
-
-  let executions = test.executions();
-
-  let [execution] = executions.as_slice() else {
-    panic!("expected one execution, got {executions:?}");
-  };
-
-  assert!(execution.timestamp_ns > 0);
-
-  let directory = execution.directory.as_ref().unwrap();
-
-  assert_eq!(
-    directory.canonicalize().unwrap(),
-    test.path("").canonicalize().unwrap(),
-  );
-
-  assert_eq!(
-    execution,
-    &Execution {
-      directory: Some(directory.clone()),
-      ..Execution::new("foo", execution.timestamp_ns)
-    },
-  );
+  Test::new()
+    .arguments(["add", "--", "foo"])
+    .success()
+    .assert_recorded();
 }
 
 #[test]
